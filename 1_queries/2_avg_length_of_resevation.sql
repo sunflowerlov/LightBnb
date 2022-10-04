98856f9f4faa06c06c40016db67434ba9c52513b
Lisha Liang <lishaliang1999@gmail.com>
2022-10-04 15:36:29 +0000

    queries

---
+++ 1_queries/2_avg_length_of_resevation.sql
@@ -0,0 +1,2 @@
+SELECT avg(end_date - start_date) as avg_duration
+FROM reservations;
--- seeds/01_seeds.sql
+++ seeds/01_seeds.sql
@@ -1,7 +1,7 @@
 INSERT INTO users (name, email, password)
 VALUES ('Eva Stanley', 'sebastianguerra@ymail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'),
 ('Louisa Meyer', 'jacksonrose@hotmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'),
-('Dominic Parks', 'victoriablackwell@outlook.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.')
+('Dominic Parks', 'victoriablackwell@outlook.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
 
 INSERT INTO properties (
   owner_id,
@@ -10,9 +10,9 @@ INSERT INTO properties (
   thumbnail_photo_url,
   cover_photo_url,
   cost_per_night,
-  parking_space,
-  number_of_bathroom,
-  number_of_bedroom,
+  parking_spaces,
+  number_of_bathrooms,
+  number_of_bedrooms,
   country,
   street,
   city,
@@ -73,10 +73,10 @@ VALUES (
 );
 
 
-INSERT INTO reservations (start_date, end_date, property_id, guest_id)
-VALUES ('2018-09-11', '2018-09-26', 2, 3),
-('2019-01-04', '2019-02-01', 2, 2),
-('2021-10-01', '2021-10-14', 1, 3);
+INSERT INTO reservations (id, start_date, end_date, property_id, guest_id)
+VALUES (1, '2018-09-11', '2018-09-26', 2, 3),
+(2, '2019-01-04', '2019-02-01', 2, 2),
+(3, '2021-10-01', '2021-10-14', 1, 3);
 
 INSERT INTO property_reviews (guest_id, property_id, reservation_id, rating, message)
 VALUES (3, 2, 1, 3, 'message'),
